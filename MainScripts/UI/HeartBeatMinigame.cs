6803372cf39c0569b26fd4849ebae71c438b30c6
Mpkiwi <masonparkie@gmail.com>
2022-09-27 09:43:57 +1300

    Fixed Z & X axis movment on Breathing Minigame, Cleaned Heartbeat to change transparency instead of disabling gameobject(future use).

--- MainScripts/UI/HeartBeatMinigame.cs
+++ MainScripts/UI/HeartBeatMinigame.cs
@@ -8,13 +8,14 @@ using Random = UnityEngine.Random;
 public class HeartBeatMinigame : MonoBehaviour
 {
     [Header("Debug:")]
-    [SerializeField] private int score = 0;
+    [SerializeField] public int score = 0;
     [SerializeField] private float time = 0f;
     private bool spaceDown = false;
     //private bool isFaded = true;
 
     [Header("Required Refs:")]
     public MinigameManger MinigameManger;
+    public HeartBeatWinCheck lineWinCheck;
     public Transform mainZone;
     public Transform leftZone;
     public Transform rightZone;
@@ -22,6 +23,7 @@ public class HeartBeatMinigame : MonoBehaviour
     public Transform endpoint;
     public GameObject redLineIndicator;
     public Image heartBeat;
+    private Image redlineimg; 
 
     [Header("Configs:")]
     public float mainZoneRange = 25f;
@@ -34,6 +36,7 @@ public class HeartBeatMinigame : MonoBehaviour
     public float maxCooldown = 10f;
     public float length = 120f;
 
+   
     public void HeartBeatStart()
     {
         StopAllCoroutines();
@@ -50,6 +53,7 @@ public class HeartBeatMinigame : MonoBehaviour
     }
 public IEnumerator HeartBeatEvent()
     {
+        Image redlineimg = redLineIndicator.GetComponent(typeof(Image)) as Image;
         StartCoroutine(Timer());
         while (time < length)
         {
@@ -58,15 +62,20 @@ public IEnumerator HeartBeatEvent()
             Debug.Log("Finished Cooldown: " + (Mathf.Round(cooldown * 100) / 100) + "s");
             spaceDown = false;
             redLineIndicator.transform.position = resetPoint.position;
-            redLineIndicator.SetActive(true);
+            Color c2 = redlineimg.color;
+            float alpha2 = 1f;
+            c2.a = alpha2;
+            redlineimg.color = c2;
             Color c = heartBeat.color;
             float alpha = 1f;
             c.a = alpha;
             heartBeat.color = c;
             StartCoroutine(Transform());
             yield return Transform();
-            redLineIndicator.SetActive(false);
             WinCheck();
+            alpha2 = 0f;    
+            c2.a = alpha2;
+            redlineimg.color = c2;
             StartCoroutine(Fade());
             yield return Fade();
             heartBeat.fillAmount = 0;
@@ -76,7 +85,7 @@ public IEnumerator HeartBeatEvent()
 
     public IEnumerator Transform()
     {
-        while (redLineIndicator.transform.position.x < endpoint.position.x && !spaceDown)
+        while (redLineIndicator.transform.localPosition.x < endpoint.localPosition.x && !spaceDown)
         {
             if (Keyboard.current.spaceKey.wasPressedThisFrame)
             {
@@ -110,7 +119,7 @@ public IEnumerator HeartBeatEvent()
             score += sideZonePointGain;
             Debug.Log("Hit Side zone, Adding " + sideZonePointGain + " point/s. New point total of: " + score);
         }
-        else 
+        else
         {
             score += missedZonesPointLoss;
             Debug.Log("Missed Zones, Removing " + missedZonesPointLoss + " point/s. New point total of: " + score);
